9c8161c76a36f25a6797ae26d7c75081b328b7f4
Ade1fe <addypearl09@gmail.com>
2023-02-28 22:11:43 +0100

    added some section

--- Anime/app.js
+++ Anime/app.js
@@ -4,6 +4,8 @@
     (window.scrollY >= navOffset) ? navbar.classList.add("fixed-nav") : navbar.classList.remove("fixed-nav")
   });
 
+
+  // notification Bar
   
     $(document).ready(() => {
   $(".carl").hide();
@@ -12,3 +14,54 @@
     $(".carl").toggle();
   });
 });
+
+
+
+
+// tablinks
+
+const tabLinks = document.getElementsByClassName("tab-links");
+const tabContents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tabLink of tabLinks){
+        tabLink.classList.remove('active-link');
+    }
+    for(tabContent of tabContents){
+        tabContent.classList.remove('active-tab');
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+
+
+// season carousel
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-red", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-red";
+}
