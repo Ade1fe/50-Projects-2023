29c4a0b3dbe177086d44bfa304ff684d9a681860
Ade1fe <addypearl09@gmail.com>
2023-05-05 20:03:37 +0100

    carousel

--- Schhol Website/js/script1.js
+++ Schhol Website/js/script1.js
@@ -1,2 +1,14 @@
 
 
+
+    var mySwiperOne = new Swiper(".mySwiperOne", {
+      pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
