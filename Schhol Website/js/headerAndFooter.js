cb5245399087ed74cce5ab502cb39472eaec2bd2
Ade1fe <addypearl09@gmail.com>
2023-05-07 17:03:34 +0100

    contact

--- Schhol Website/js/headerAndFooter.js
+++ Schhol Website/js/headerAndFooter.js
@@ -13,3 +13,37 @@ cancelIcon.addEventListener("click", () => {
   menuIcon.style.display = "block";
   cancelIcon.style.display = "none";
 });
+
+
+
+
+const buyNow = document.querySelector('.buy-now');
+
+window.addEventListener('scroll', function() {
+  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  if (scrollTop > 0) {
+    buyNow.classList.add('buy-now--fixed');
+  } else {
+    buyNow.classList.remove('buy-now--fixed');
+  }
+});
+
+
+
+
+const scrollToTopButton = document.getElementById('scroll-to-top');
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopButton.style.display = 'block';
+  } else {
+    scrollToTopButton.style.display = 'none';
+  }
+}
+
+// Scroll to the top of the document when the button is clicked
+scrollToTopButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
